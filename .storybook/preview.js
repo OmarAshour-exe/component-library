b22fc7fa76cd21f7257791b5f6c9d4d23f132f52
Omar Ashour <omar.ashoor963@gmail.com>
2023-10-24 14:03:17 +0200

    integrated pandaCSS

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,11 @@
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+import "../app/global.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
 };
-
-export default preview;
--- components/checkbox/Checkbox.js
+++ components/checkbox/Checkbox.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './checkbox.css'; // Import your CSS file
 
 function Checkbox({ label, checked, onChange }) {
     return (
-        <label>
-            <input type="checkbox" checked={checked} onChange={onChange} />
+        <label className="custom-checkbox">
+            <input checked={checked} onChange={onChange} type="checkbox" />
             {label}
         </label>
     );
